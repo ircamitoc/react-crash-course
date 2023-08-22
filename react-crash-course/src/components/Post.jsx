275722e3c7febfadcd1c6d5513de99b159f3365b
ircamitoc <iraincamitoc@gmail.com>
2023-08-23 02:06:08 +0800

    Removed asset folder and added code

--- react-crash-course/src/components/Post.jsx
+++ react-crash-course/src/components/Post.jsx
@@ -1,8 +1,10 @@
 function Post() {
-    return <div>
-        <p>Maximilian</p>
-        <p>React.js is awesome!</p>
+  return (
+    <div>
+      <p>Maximilian</p>
+      <p>React.js is awesome!</p>
     </div>
+  );
 }
 
 export default Post;
