b28d8cd421fb72644e893c1e17044f2578cb899e
ircamitoc <iraincamitoc@gmail.com>
2023-08-23 21:05:47 +0800

    Changed port to post

--- react-crash-course/src/components/Post.jsx
+++ react-crash-course/src/components/Post.jsx
@@ -2,7 +2,7 @@ import classes from "./Post.module.css";
 
 function Post(props) {
   return (
-    <li className={classes.port}>
+    <li className={classes.post}>
       <p className={classes.author}>{props.author}</p>
       <p className={classes.text}>{props.body}</p>
     </li>
