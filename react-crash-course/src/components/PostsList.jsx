c50c3be77872b6bc1fe7615401361bb0e5fee997
ircamitoc <iraincamitoc@gmail.com>
2023-08-23 20:07:36 +0800

    Added file and code

---
+++ react-crash-course/src/components/PostsList.jsx
@@ -0,0 +1,10 @@
+function PostsList() {
+  return (
+    <ul>
+      <Post author="Maximilian" body="React.js is awesome!" />
+      <Post author="Manuel" body="Check out the full course!" />
+    </ul>
+  );
+}
+
+export default PostsList;
