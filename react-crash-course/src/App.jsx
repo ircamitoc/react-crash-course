2a08fcde2306c5ab7b7ea46a2232134a6f98db23
ircamitoc <iraincamitoc@gmail.com>
2023-08-23 20:19:07 +0800

    Removed authors and added PostList as syntax

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -3,8 +3,7 @@ import PostsList from "./components/PostsList";
 function App() {
   return (
     <main>
-      <Post author="Maximilian" body="React.js is awesome!" />
-      <Post author="Manuel" body="Check out the full course!" />
+      <PostsList />
     </main>
   );
 }
