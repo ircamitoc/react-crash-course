06fdeaef1ccbd187cbea8cda68b33c50f667dd8e
ircamitoc <iraincamitoc@gmail.com>
2023-08-23 19:48:06 +0800

    Added post authors

--- react-crash-course/src/components/Post.jsx
+++ react-crash-course/src/components/Post.jsx
@@ -2,10 +2,10 @@ import classes from "./Post.module.css";
 
 function Post(props) {
   return (
-    <div className={classes.port}>
+    <li className={classes.port}>
       <p className={classes.author}>{props.author}</p>
       <p className={classes.text}>{props.body}</p>
-    </div>
+    </li>
   );
 }
 
