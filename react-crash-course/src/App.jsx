42d548e71f604fc46388d747a8be75457dcb003a
ircamitoc <iraincamitoc@gmail.com>
2023-08-24 00:47:22 +0800

    Added new header

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import PostsList from "./components/PostsList";
 import MainHeader from "./components/MainHeader";
 
 function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   return (
     <>
       <MainHeader />
